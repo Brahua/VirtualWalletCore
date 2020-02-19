bb63f3cd91392dc068c9ce10a7b95f1237175275
Jerlouis Vivas <jerlouisvivas@gmail.com>
2020-02-18 23:10:04 -0500

    .

--- VirtualWalletCore.playground/Contents.swift
+++ VirtualWalletCore.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 var persona = Persona(nombre: "Josue", apellido: "Bravo")
-persona.cuenta = Cuenta(cuenta: 0, descripcion: "Enero")
+persona.cuenta = Cuenta(cuenta: 0, descripcion: "Cuenta Personal")
 
 let alimentoSnow = Presupuesto(categoria: .alimentoSnow, cantidad: 150, descripcion: "Comida snow")
 let alimento = Presupuesto(categoria: .alimento, cantidad: 300, descripcion: "Comida")
@@ -20,155 +20,87 @@ persona.cuenta?.agregarPresupuesto(celular)
 persona.cuenta?.agregarPresupuesto(entretenimiento)
 persona.cuenta?.agregarPresupuesto(renta)
 persona.cuenta?.agregarPresupuesto(estudio)
-persona.cuenta?.agregarPresupuesto(ahorro)
-persona.cuenta?.agregarPresupuesto(presupuestoExtra)
 
 
-// Sueldo
-do {
-    try persona.cuenta?.agregarTransaccion(
-        .ingreso(
-            cantidad: 1500,
-            descripcion: "Sueldo Mensual",
-            fecha: Date(anio: 2020, mes: 01, dia: 07)
+func agregarIngreso(cantidad: Float, descripcion: String, fecha: Date){
+    do {
+        try persona.cuenta?.agregarTransaccion(
+            .ingreso(cantidad: cantidad, descripcion: descripcion, fecha: fecha)
         )
-    )
-} catch  {
-    print("Error al agregar el sueldo.", error)
+    } catch {
+        print("No se puedo generar la transacción: \(descripcion):S/.\(cantidad)")
+    }
 }
 
-
-// Gastos mensuales
-do {
-    try persona.cuenta?.agregarTransaccion(
-        .gasto(
-            cantidad: 80,
-            descripcion: "Casa",
-            categoria: .renta,
-            fecha: Date(anio: 2020, mes: 01, dia: 07)
+func agregarGasto(cantidad: Float, descripcion: String, categoria: CategoriaGasto, fecha: Date){
+    do {
+        try persona.cuenta?.agregarTransaccion(
+            .gasto(cantidad: cantidad, descripcion: descripcion, categoria: categoria, fecha: fecha)
         )
-    )
-} catch {
-    print("Casa", error)
+    } catch CuentaException.presupuestoExcedido {
+        print("\(descripcion): El gasto S/.\(cantidad) excede del presupuesto establecido.")
+    } catch CuentaException.cantidadExcedida {
+        print("\(descripcion): El gasto S/.\(cantidad) excede su cuenta personal.")
+    } catch {
+        print("No se puedo generar la transacción: \(descripcion):S/.\(cantidad)")
+    }
 }
 
-do {
-    try persona.cuenta?.agregarTransaccion(
-        .gasto(
-            cantidad: 44.90,
-            descripcion: "Netflix",
-            categoria: .entretenimiento,
-            fecha: Date(anio: 2020, mes: 01, dia: 07)
-        )
-    )
-} catch {
-    print("Netflix", error)
-}
+agregarIngreso(
+    cantidad: 1500,
+    descripcion: "Sueldo Mensual",
+    fecha: Date(anio: 2020, mes: 1, dia: 7)
+)
 
-do {
-    try persona.cuenta?.agregarTransaccion(
-        .gasto(
-            cantidad: 18.90,
-            descripcion: "Spotify",
-            categoria: .entretenimiento,
-            fecha: Date(anio: 2020, mes: 01, dia: 07)
-        )
-    )
-} catch {
-    print("Spotify", error)
-}
+agregarGasto(
+    cantidad: 80,
+    descripcion: "Casa",
+    categoria: .renta,
+    fecha: Date()
+)
+agregarGasto(
+    cantidad: 44.90,
+    descripcion: "Netflix",
+    categoria: .entretenimiento,
+    fecha: Date(anio: 2020, mes: 01, dia: 07)
+)
+agregarGasto(
+    cantidad: 18.90,
+    descripcion: "Spotify",
+    categoria: .entretenimiento,
+    fecha: Date(anio: 2020, mes: 01, dia: 07)
+)
+agregarGasto(
+    cantidad: 90,
+    descripcion: "Linea telefonica",
+    categoria: .celular,
+    fecha: Date(anio: 2020, mes: 01, dia: 07)
+)
+agregarGasto(
+    cantidad: 30,
+    descripcion: "Pasaje mensual",
+    categoria: .transporte,
+    fecha: Date(anio: 2020, mes: 01, dia: 07)
+)
+agregarGasto(
+    cantidad: 100,
+    descripcion: "Curso Platzi",
+    categoria: .estudio,
+    fecha: Date(anio: 2020, mes: 01, dia: 07)
+)
+agregarGasto(
+    cantidad: (35*4),
+    descripcion: "Comida semanal para Snow",
+    categoria: .alimentoSnow,
+    fecha: Date(anio: 2020, mes: 01, dia: 07)
+)
+agregarGasto(
+    cantidad: (70*4),
+    descripcion: "Comida semanal",
+    categoria: .alimento,
+    fecha: Date(anio: 2020, mes: 01, dia: 07)
+)
 
-do {
-    try persona.cuenta?.agregarTransaccion(
-        .gasto(
-            cantidad: 90,
-            descripcion: "Linea telefonica",
-            categoria: .celular,
-            fecha: Date(anio: 2020, mes: 01, dia: 07)
-        )
-    )
-} catch {
-    print("Linea telefonica", error)
-}
-
-do {
-    try persona.cuenta?.agregarTransaccion(
-        .gasto(
-            cantidad: 30,
-            descripcion: "Pasaje mensual",
-            categoria: .transporte,
-            fecha: Date(anio: 2020, mes: 01, dia: 07)
-        )
-    )
-} catch {
-    print("Pasaje mensual", error)
-}
-
-do {
-    try persona.cuenta?.agregarTransaccion(
-        .gasto(
-            cantidad: 100,
-            descripcion: "Curso Platzi",
-            categoria: .estudio,
-            fecha: Date(anio: 2020, mes: 01, dia: 07)
-        )
-    )
-} catch {
-    print("Curso Platzi", error)
-}
-
-do {
-    try persona.cuenta?.agregarTransaccion(
-        .gasto(
-            cantidad: 300,
-            descripcion: "Ahorro",
-            categoria: .ahorro,
-            fecha: Date(anio: 2020, mes: 01, dia: 07)
-        )
-    )
-} catch {
-    print("Ahorro", error)
-}
-
-do {
-    try persona.cuenta?.agregarTransaccion(
-        .gasto(
-            cantidad: 50,
-            descripcion: "Presupuesto Extra",
-            categoria: .extra,
-            fecha: Date(anio: 2020, mes: 01, dia: 07)
-        )
-    )
-} catch {
-    print("Presupuesto Extra", error)
-}
-
-// Gastos semanales
-do {
-    try persona.cuenta?.agregarTransaccion(
-        .gasto(
-            cantidad: (35*4),
-            descripcion: "Comida semanal para Snow",
-            categoria: .alimentoSnow,
-            fecha: Date(anio: 2020, mes: 01, dia: 07)
-        )
-    )
-} catch {
-    print("Comida semanal para Snow", error)
-}
-
-do {
-    try persona.cuenta?.agregarTransaccion(
-        .gasto(
-            cantidad: (70*4),
-            descripcion: "Comida semanal",
-            categoria: .alimento,
-            fecha: Date(anio: 2020, mes: 01, dia: 07)
-        )
-    )
-} catch {
-    print("Comida semanal", error)
-}
 
 print("Gasto total: \(persona.cuenta?.obtenerGastoTotal() ?? 0) ")
 print("Dinero habil para gastar: \(persona.cuenta?.cuenta ?? 0)")
--- VirtualWalletCore.playground/Sources/Date.swift
+++ VirtualWalletCore.playground/Sources/Date.swift
@@ -2,11 +2,12 @@ import Foundation
 
 public extension Date {
     init(anio: Int, mes: Int, dia: Int) {
-        let calendario = Calendar(identifier: .gregorian)
         var dateComponents = DateComponents()
         dateComponents.year = anio
         dateComponents.month = mes
         dateComponents.day = dia
+        dateComponents.timeZone = TimeZone(abbreviation: "PET")
+        let calendario = Calendar.current
         self = calendario.date(from: dateComponents) ?? Date()
     }
 }
